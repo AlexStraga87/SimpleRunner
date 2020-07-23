965a7794c8056925bf1f30af59fc97cba1026978
AlexStraga87 <somemail87@gmail.com>
2020-07-23 20:38:51 +0300

    Рефакторинг

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    [SerializeField] private Vector3 _offsetPosition;
+    [SerializeField] private Vector3 _offset;
     [SerializeField] private Transform _player;
+    private readonly Vector3 _enabledAxis = new Vector3(1, 0, 1);
 
     private void Update()
     {
-        var newPosition = _player.position;
-        newPosition.y = 0;
-        transform.position = newPosition + _offsetPosition;
+        transform.position = Vector3.Scale(_player.position, _enabledAxis) + _offset;
     }
 }
